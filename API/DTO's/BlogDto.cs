5acce243e48eda2876d6f4ee0331a989e7b04e13
DevvKuba <jrfitnessinbox@gmail.com>
2026-05-03 23:00:22 +0100

    Complete foundational IBlogRepository methods along with BlogDto

---
+++ API/DTO's/BlogDto.cs
@@ -0,0 +1,17 @@
+﻿namespace API.DTO_s
+{
+    public class BlogDto
+    {
+        public int Id { get; set; }
+
+        public required string Description { get; set; }
+
+        public DateTime PublishedAt { get; private set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+
+        public int UserId { get; set; }
+    }
+}
--- API/Interfaces/IBlogRepository.cs
+++ API/Interfaces/IBlogRepository.cs
@@ -1,6 +1,19 @@
+using API.Entities;
+
 namespace API.Interfaces
 {
     public interface IBlogRepository
     {
+        Task<Blog> GetBlogByIdAsync(int id);
+
+        Task<Blog> GetBlogByUserIdAsync(int userId);
+
+        Task<Blog[]> GetAllUserBlogsAsync { get; set; }
+
+        Task UpdateBlogAsync(Blog blog);
+
+        Task AddBlogAsync(AppUser user, string description);
+
+        void RemoveBlog(Blog blog);
     }
 }
