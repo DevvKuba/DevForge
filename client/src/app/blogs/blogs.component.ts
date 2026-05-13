c3e2fa99604b8f2fa89149175beb9ea5e0cc70fd
DevvKuba <jrfitnessinbox@gmail.com>
2026-05-13 14:04:01 +0100

    Install compatible primeng version to enabling blog component feature implementations

--- client/src/app/blogs/blogs.component.ts
+++ client/src/app/blogs/blogs.component.ts
@@ -1,10 +1,22 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { BlogService } from '../_services/blog.service';
 import { Blog } from '../_models/blog';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Member } from '../_models/member';
+import { AutoCompleteModule } from 'primeng/autocomplete';
 
 @Component({
   selector: 'app-blogs',
-  imports: [],
+  imports: [
+    CardModule, 
+    ButtonModule, 
+    AutoCompleteModule, 
+    CommonModule, 
+    FormsModule
+  ],
   templateUrl: './blogs.component.html',
   styleUrl: './blogs.component.css'
 })
@@ -13,6 +25,7 @@ export class BlogsComponent implements OnInit {
   pageNumber : number = 1;
   pageSize : number = 5;
   blogs: Blog[] = [];
+  members: Member[] = [];
 
   ngOnInit(): void {
     this.blogService.gatherAllBlogs(this.pageNumber, this.pageSize).subscribe({
