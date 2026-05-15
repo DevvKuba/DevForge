84c7f8afbc24819e37cf911424f8aa5aef0edccf
DevvKuba <jrfitnessinbox@gmail.com>
2026-05-15 13:20:21 +0100

    Create and apply migrations asserting new user Id property within blog comment and like entities

---
+++ API/Data/Migrations/20260515121928_AddUserIdToBlogLikeAndBlogComment.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToBlogLikeAndBlogComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "BlogLikes",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "BlogComments",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "BlogLikes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "BlogComments");
+        }
+    }
+}
