84e7df7e4e9fbb4b02bed206e51c43f81ee30183
DevvKuba <jrfitnessinbox@gmail.com>
2026-05-18 13:55:14 +0100

    Implement html template for member specific blogs as well as adequate styling

--- client/src/app/members/member-blogs/member-blogs.component.ts
+++ client/src/app/members/member-blogs/member-blogs.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { BlogService } from '../../_services/blog.service';
+import { Blog } from '../../_models/blog';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-member-blogs',
-  imports: [],
+  imports: [
+    CardModule,
+    CommonModule,
+  ],
   templateUrl: './member-blogs.component.html',
   styleUrl: './member-blogs.component.css'
 })
 export class MemberBlogsComponent {
+  userId: number = 0;
+
+  private blogService = inject(BlogService);
+  private route = inject(ActivatedRoute);
+
+  blogs: Blog[] = [];
+  pageNumber = 1;
+  pageSize = 4;
 
 }
