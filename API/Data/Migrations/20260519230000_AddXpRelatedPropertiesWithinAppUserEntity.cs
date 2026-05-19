3339c52b1da4d5d23186107ff098ac098b046e58
DevvKuba <jrfitnessinbox@gmail.com>
2026-05-20 00:02:14 +0100

    Create migrations containing new Xp related app user properties, and apply database update

---
+++ API/Data/Migrations/20260519230000_AddXpRelatedPropertiesWithinAppUserEntity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddXpRelatedPropertiesWithinAppUserEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AppExperiencePoints",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLevelUpDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Level",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppExperiencePoints",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastLevelUpDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "AspNetUsers");
+        }
+    }
+}
