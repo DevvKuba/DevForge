af0813e93c37860ef69d991b151bdaaeebd99be6
DevvKuba <jrfitnessinbox@gmail.com>
2026-05-19 18:48:15 +0100

    Declare and implement new IXpService method within service class

---
+++ API/Interfaces/IXpService.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Interfaces
+{
+    public interface IXpService
+    {
+        int GetXpThresholdForLevel(int level); // IMP total exp for that level
+
+        int GetRemainingXpForNextLevel(int totalXp, int level); // until next level
+
+        int GetOverflowXpAfterLevelUp(int totalXp, int pastLevel); 
+
+        int AwardXp(int totalXp, int level);
+
+        bool hasLevelUpOccured(int totalXp, int level);
+
+    }
+}
---
+++ API/Services/XpService.cs
@@ -0,0 +1,32 @@
+﻿using API.Interfaces;
+
+namespace API.Services
+{
+    public class XpService : IXpService
+    {
+        public int AwardXp(int totalXp, int level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetOverflowXpAfterLevelUp(int totalXp, int pastLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetRemainingXpForNextLevel(int totalXp, int level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetXpThresholdForLevel(int level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool hasLevelUpOccured(int totalXp, int level)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
