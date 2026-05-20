137c70f5d69dce5c25c2ce34cf4de627061922a7
DevvKuba <jrfitnessinbox@gmail.com>
2026-05-20 14:29:23 +0100

    Close out level update migration ensuring all app user entites start at the inteded level 1

---
+++ API/Data/Migrations/20260520132410_ChangeAppUserLevelToStartingOneValue.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeAppUserLevelToStartingOneValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE AspNetUsers SET Level = 1 WHERE Level = 0 OR Level IS NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE AspNetUsers SET Level = 0");
+        }
+    }
+}
