f3c8627bc487a9d11c5ace868ddf7628de7c174e
DevvKuba <jrfitnessinbox@gmail.com>
2026-05-22 19:42:21 +0100

    Introduce ApiResponse object, used as consistent response object, along with UserXpDetailDto

---
+++ API/DTO's/UserXpDetailDto.cs
@@ -0,0 +1,11 @@
+﻿namespace API.DTO_s
+{
+    public class UserXpDetailDto
+    {
+        public int Level { get; set; }
+
+        public int CurrentXp { get; set; }
+
+        public int LevelThreshold { get; set; }
+    }
+}
---
+++ API/Helpers/ApiResponse.cs
@@ -0,0 +1,16 @@
+﻿using API.DTO_s;
+
+namespace API.Helpers
+{
+    public class ApiResponse<T>
+    {
+        public T? Data { get; set; }
+
+        public string? Message { get; set; }
+
+        public bool Success { get; set; }
+
+        public required UserXpDetailDto XpDetails { get; set; }
+
+    }
+}
