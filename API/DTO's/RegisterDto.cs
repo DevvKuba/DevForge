23db82b83c65800ab3474e601572049475820cb4
DevvKuba <jrfitnessinbox@gmail.com>
2026-05-22 23:56:59 +0100

    Supply and extend specific error message as part of existing data annotations within RegisterDto

--- API/DTO's/RegisterDto.cs
+++ API/DTO's/RegisterDto.cs
@@ -4,21 +4,31 @@ namespace API.DTO_s
 {
     public class RegisterDto
     {
-        [Required] public string Username { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Username must be provided")] 
+        public string Username { get; set; } = string.Empty;
 
-        [Required] public string? KnownAs { get; set; }
+        [Required(ErrorMessage = "Known as property must be provided")]
+        public string? KnownAs { get; set; }
 
-        [Required] public string? Gender { get; set; }
+        [Required(ErrorMessage = "Gender must be provided")]
+        public string? Gender { get; set; }
 
-        [Required] public string? DateOfBirth { get; set; }
+        [Required(ErrorMessage = "Date of birth must be provided")]
+        public string? DateOfBirth { get; set; }
 
-        [Required] public string? Specialization { get; set; }
+        [Required(ErrorMessage = "Specializaion must be provided")]
+        public string? Specialization { get; set; }
 
-        [Required] public int? yearsOfExperience { get; set; }
+        [Required(ErrorMessage = "Years of experience must be provided")]
+        public int? yearsOfExperience { get; set; }
 
 
-        [Required]
-        [StringLength(16, MinimumLength = 4)]
+        [Required(ErrorMessage = "New password is required")]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Password needs to be at least 8 characters")]
+        [RegularExpression(
+        @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$",
+        ErrorMessage = "Password must contain uppercase, lowercase, number, and special character.")]
         public string Password { get; set; } = string.Empty;
     }
 }
