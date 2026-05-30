29ede2322de7a957340819b1f82b079e24a93394
DevvKuba <jrfitnessinbox@gmail.com>
2026-05-30 23:50:35 +0100

    Implement new quiz and quiz question entities, finalising quiz question properties

---
+++ API/Entities/QuizQuestion.cs
@@ -0,0 +1,17 @@
+﻿namespace API.Entities
+{
+    public class QuizQuestion
+    {
+        public int Id { get; set; }
+
+        public int QuizId { get; set; }
+
+        public required string Question { get; set; }
+
+        public required string CorrectAnswer { get; set; }
+
+        public required List<string> IncorrectAnswers { get; set; }
+
+        public required string  Difficulty { get; set; }
+    }
+}
