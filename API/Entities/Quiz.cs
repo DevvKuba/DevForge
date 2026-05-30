a7581e281a74ce1fea9b696c7982f7b413650ef6
DevvKuba <jrfitnessinbox@gmail.com>
2026-05-30 23:50:55 +0100

    Complete Quiz entity properties

--- API/Entities/Quiz.cs
+++ API/Entities/Quiz.cs
@@ -2,6 +2,19 @@
 {
     public class Quiz
     {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public required string Topic { get; set; } // change to enum
+
+        public required string Difficulty { get; set; } // change to enum
+
+        public required List<QuizQuestion> Questions { get; set; }
+
+        public int Score { get; set; }
+
+        public DateTime CompletedAt { get; set; }
 
     }
 }
