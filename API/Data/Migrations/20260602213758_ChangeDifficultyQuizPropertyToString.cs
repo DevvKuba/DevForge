c5e3b1ed9391c0adf6ba29abb7124a707e47967c
DevvKuba <jrfitnessinbox@gmail.com>
2026-06-02 22:54:00 +0100

    Create and apply database migrations altering Quiz Difficulty property from past enum type to string

---
+++ API/Data/Migrations/20260602213758_ChangeDifficultyQuizPropertyToString.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDifficultyQuizPropertyToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Difficulty",
+                table: "Quizzes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Difficulty",
+                table: "Quizzes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
